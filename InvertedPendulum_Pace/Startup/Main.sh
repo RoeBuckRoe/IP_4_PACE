ce43af3829aa0781d44c5a4a5ef0fb61840d7479
Achyuth Ashok <72122247+RoeBuckRoe@users.noreply.github.com>
2025-03-13 11:08:17 +0000

    PACE app and BalanceLQRGAM

--- InvertedPendulum_Pace/Startup/Main.sh
+++ InvertedPendulum_Pace/Startup/Main.sh
@@ -132,9 +132,12 @@ LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MARTe2_Components_DIR/Build/$TARGET/Components
 
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/superuser/MARTe_ws/InvertedPendulum-MARTe2/Build/x86-linux/Components/GAMs/HomingGAM/
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/superuser/MARTe_ws/InvertedPendulum-MARTe2/Build/x86-linux/Components/GAMs/IPStatusGAM/
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/superuser/MARTe_ws/InvertedPendulum-MARTe2/Build/x86-linux/Components/GAMs/StartupGAM
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/superuser/MARTe_ws/InvertedPendulum-MARTe2/Build/x86-linux/Components/GAMs/SwingUpGAM
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/superuser/MARTe_ws/InvertedPendulum-MARTe2/Build/x86-linux/Components/GAMs/StartupGAM/
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/superuser/MARTe_ws/InvertedPendulum-MARTe2/Build/x86-linux/Components/GAMs/SwingUpGAM/
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/superuser/MARTe_ws/InvertedPendulum-MARTe2/Build/x86-linux/Components/DataSources/MotorSTM32/
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/superuser/MARTe_ws/InvertedPendulum-MARTe2/Build/x86-linux/Components/GAMs/BalanceGAM/
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/superuser/MARTe_ws/InvertedPendulum-MARTe2/Build/x86-linux/Components/GAMs/ResetGAM/
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/superuser/MARTe_ws/InvertedPendulum-MARTe2/Source/Components/GAMs/BalanceLQRGAM/
 
 
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../Build/$TARGET/Components/DataSources/EduKitSerial
